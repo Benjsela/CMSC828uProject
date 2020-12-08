302f46c820562285caa6c4526f3d1bc0db231f63
Mara Levy <mlevy@vulcan21.umiacs.umd.edu>
2020-12-08 17:24:57 -0500

    hi

--- networks/starter.py
+++ networks/starter.py
@@ -20,15 +20,17 @@ from torchvision import datasets, transforms
 class CNN(nn.Module):
     def __init__(self):
         super(CNN, self).__init__()
-        self.conv1 = nn.Conv2d(1, 10, kernel_size=5)
-        self.conv2 = nn.Conv2d(10, 20, kernel_size=5)
+        self.conv1 = nn.Conv2d(1, 10, kernel_size=5, stride=2)
+        self.conv2 = nn.Conv2d(10, 20, kernel_size=5, stride=2)
         self.conv2_drop = nn.Dropout2d()
         self.fc1 = nn.Linear(320, 50)
         self.fc2 = nn.Linear(50, 10)
 
     def forward(self, x):
-        x = F.relu(F.max_pool2d(self.conv1(x), 2))
-        x = F.relu(F.max_pool2d(self.conv2_drop(self.conv2(x)), 2))
+        #x = F.relu(F.max_pool2d(self.conv1(x), 2))
+        #x = F.relu(F.max_pool2d(self.conv2_drop(self.conv2(x)), 2))
+        x = F.relu(self.conv1(x))
+        x = F.relu(self.conv2_drop(self.conv2(x)))
         x = x.view(-1, 320)
         x = F.relu(self.fc1(x))
         x = F.dropout(x, training=self.training)
