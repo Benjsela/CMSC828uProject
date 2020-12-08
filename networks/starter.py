43517e996a9e0e29bed466b93219df5acc899ba4
Mara Levy <mlevy@vulcan00.umiacs.umd.edu>
2020-12-08 16:50:09 -0500

    train

--- networks/starter.py
+++ networks/starter.py
@@ -103,6 +103,6 @@ def main():
         train(model, device, train_loader, optimizer, epoch, log_interval)
         test(model, device, test_loader)
 
-    torch.save(model.state_dict(), "trained_networks/trained_starter")
+    torch.save({'epoch': epoch, 'model_state_dict': model.state_dict(), 'optimizer_state_dict': optimizer.state_dict()}, "trained_networks/trained_starter")
 if __name__ == '__main__':
     main()
